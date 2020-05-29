0ad96dbe6a6bf6d9fd7008aec3c1854ba0e89ffd
Shilpi Mittal <56123020+shilpi1998@users.noreply.github.com>
2020-05-29 22:53:09 +0530

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,15 @@
+for file in  *.jpg 
+
+do 
+
+year=$(echo $file | cut -d'-' -f 1)
+
+month=$(echo $file | cut -d'-' -f 2)
+e
+cho  $year $month
+
+mkdir -p  $year/$month
+
+mv $file $year/$month
+
+done
